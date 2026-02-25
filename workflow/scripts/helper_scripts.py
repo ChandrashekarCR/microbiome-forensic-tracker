96fb18d943697cec7adf97af6b1f236140aad550
ChandrashekarCR <shekarlp18@gmail.com>
2026-02-25 14:19:34 +0100

    Minor changes to helper scripts, removed unecessary functions.

--- workflow/scripts/helper_scripts.py
+++ workflow/scripts/helper_scripts.py
@@ -53,35 +53,6 @@ def load_sample_sheet(tsv_path: str, max_samples: int = None) -> pd.DataFrame:
     
     return df
 
-def get_fastq_samples(samples_dir, read1_suffix, read2_suffix):
-    """
-    Get the list of samples from the FASTQ Directory
-    
-    :param samples_dir: Path to the directory containing the fastq files
-    :param read1_suffix: Suffix for R1 files (something like "_R1.fastq.gz")
-    """
-
-    # Reads all the 
-    pattern = os.path.join(samples_dir, f"*{read1_suffix}")
-    files = glob.glob(pattern)
-
-    if not files:
-        raise ValueError(
-            f"No files found matching the patter: {pattern}\n"
-            f"Check you samples_dir and read1_suffix in the config.yaml file."
-        )   
-
-    samples = []
-    for file in files:
-        #print(os.path.basename(file))
-        basename = os.path.basename(file)
-        sample_name = basename.replace(read1_suffix,"")
-        #forward_read = file
-        #reverse_read = os.path.join(samples_dir, f"{sample_name}{read2_suffix}")
-        #samples.append([(forward_read,reverse_read)]) # For now we just retrun 5 samples to see if the pipeline is working.
-        samples.append(sample_name)
-    return samples
-
 # Get only the read 1 for a sample
 def get_sample_r1(sample_df:pd.DataFrame, sample:str) -> str:
     return sample_df.loc[sample,"r1"]
