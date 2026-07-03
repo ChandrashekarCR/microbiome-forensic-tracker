1c5cdab3c81508d8b63330c42e43e4be475ac51d
ChandrashekarCR <shekarlp18@gmail.com>
2026-07-03 19:46:03 +0200

    Linted and formatted.

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,17 +1,17 @@
 from collections.abc import AsyncGenerator
-from pathlib import Path
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
 from .config import settings
 
 # Get the project root directory (where databases/ is located)
-#PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent  # /home/chandru/binp51
-#DATABASE_DIR = PROJECT_ROOT / "databases"
-#DATABASE_DIR.mkdir(exist_ok=True)  # Ensure the databases folder exists
+# PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent  # /home/chandru/binp51
+# DATABASE_DIR = PROJECT_ROOT / "databases"
+# DATABASE_DIR.mkdir(exist_ok=True)  # Ensure the databases folder exists
 #
-#URL_DATABASE = f"sqlite+aiosqlite:///{DATABASE_DIR / 'malmo_backend.db'}"
+# URL_DATABASE = f"sqlite+aiosqlite:///{DATABASE_DIR / 'malmo_backend.db'}"
 
 
 class Base(DeclarativeBase):
@@ -22,9 +22,9 @@ class Base(DeclarativeBase):
 async_url = settings.database_url_async
 is_async_sqlite = async_url.startswith("sqlite")
 if is_async_sqlite:
-    engine = create_async_engine(async_url, echo=False,connect_args={"check_same_thread":False})
+    engine = create_async_engine(async_url, echo=False, connect_args={"check_same_thread": False})
 else:
-    engine = create_async_engine(async_url,echo=False)
+    engine = create_async_engine(async_url, echo=False)
 
 
 # Each request opens a session, uses the database and then closes it.
@@ -48,7 +48,7 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 sync_url = settings.database_url_sync
 is_sqlite = sync_url.startswith("sqlite")
 if is_sqlite:
-    sync_engine = create_engine(sync_url, connect_args={"check_same_thread": False}) # connect args is only needed for SQLite database
+    sync_engine = create_engine(sync_url, connect_args={"check_same_thread": False})  # connect args is only needed for SQLite database
 else:
     sync_engine = create_engine(sync_url)
 
